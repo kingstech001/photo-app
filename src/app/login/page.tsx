c658fbe9c8980dc07658e40f3f0f8a8884f2cdb2
kingstech001 <mamahkingsleychukwuebuka@gmail.com>
2024-09-11 12:21:35 -0700

    first commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,65 +1,63 @@
-// app/login/page.tsx
 'use client';
 
 import { useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { setTimeout } from 'timers/promises';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
 
-    const handleLogin = async (e: React.FormEvent) => {
-        e.preventDefault();
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email,
-            password,
-        });
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-        if (!error) {
-            router.push('/photos'); // Redirect to photos page after login
-        } else {
-            setError('Invalid login credentials');
-            console.error(error.message);
-        }
-    };
+    if (!error) {
+      router.push('/photos'); // Redirect to photos page after login
+    } else {
+      console.error(error.message);
+      setError('Invalid login credentials');
+    }
+  };
 
-    return (
-        <div className='flex justify-center items-center h-screen text-[12px] md:text-[18px]'>
-            <div className='w-[400px] p-6 border-y-2 border-y-orange-500 rounded-lg shadow'>
-            <h2 className='font-bold text-5 mb-5'>Log In</h2>
-                <form onSubmit={handleLogin} className=' space-y-4  mb-2'>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Email"
-                        required
-                        className='block border w-full p-2 text-[12px] rounded-md focus:outline-none border-none bg-gray-200'
-                    />
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                        required
-                        className='block border w-full p-2 text-[12px] rounded-md focus:outline-none border-none bg-gray-200'
-                    />
-                    <button type="submit" className='bg-orange-500 text-white w-full py-1 rounded-md'>Log In</button>
-                </form>
-                <div className='flex items-center justify-between'>
-                    <p>Don&apos;t have an account?</p>
-                    <Link href='/signup' className='hover:text-orange-500'>Sign Up</Link>
-                </div>
-                {error && <p className='text-red-600 text-[12px]'>{error}</p>}
-            </div>
+  return (
+    <div className='flex justify-center items-center h-screen text-[12px] md:text-[18px]'>
+      <div className='w-[400px] p-6 border-y-2 border-y-orange-500 rounded-lg shadow'>
+        <h2 className='font-bold text-5 mb-5'>Log In</h2>
+        <form onSubmit={handleLogin} className='space-y-4 mb-2'>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+            className='block border w-full p-2 text-[12px] rounded-md focus:outline-none border-none bg-gray-200'
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+            className='block border w-full p-2 text-[12px] rounded-md focus:outline-none border-none bg-gray-200'
+          />
+          <button type="submit" className='bg-orange-500 text-white w-full py-1 rounded-md'>Log In</button>
+        </form>
+        <div className='flex items-center justify-between'>
+          <p>Don&apos;t have an account?</p>
+          <Link href='/signup' className='hover:text-orange-500'>Sign Up</Link>
         </div>
-    );
+        {error && <p className='text-red-600 text-[12px]'>{error}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,3 @@
-// app/signup/page.tsx
 'use client';
 
 import { useState } from 'react';
@@ -17,7 +16,7 @@ const Signup = () => {
     e.preventDefault();
 
     // Sign up the user
-    const { data, error } = await supabase.auth.signUp({
+    const { error } = await supabase.auth.signUp({
       email,
       password,
       options: {
@@ -29,7 +28,17 @@ const Signup = () => {
       router.push('/login'); // Redirect to login page after successful signup
     } else {
       console.error(error.message);
-      setError(error.message);
+      // Handle different error types
+      switch (error.message) {
+        case 'Invalid email format':
+          setError('Please enter a valid email.');
+          break;
+        case 'Password should be at least 6 characters':
+          setError('Password should be at least 6 characters long.');
+          break;
+        default:
+          setError('An error occurred. Please try again.');
+      }
     }
   };
 
@@ -37,7 +46,7 @@ const Signup = () => {
     <div className='flex justify-center items-center h-screen text-[12px] md:text-[18px]'>
       <div className='w-[400px] p-6 border-y-2 border-y-orange-500 rounded-lg shadow'>
         <h2 className='font-bold text-5 mb-5'>Sign Up</h2>
-        <form onSubmit={handleSignup} className=' space-y-4  mb-2'>
+        <form onSubmit={handleSignup} className=' space-y-4 mb-2'>
           <input
             type="text"
             value={name}
@@ -63,12 +72,12 @@ const Signup = () => {
             className='block border w-full p-2 text-[12px] rounded-md focus:outline-none border-none bg-gray-200'
           />
           <button type="submit" className='bg-orange-500 text-white w-full py-1 rounded-md'>Sign Up</button>
-          {error && <p>{error}</p>}
+          {error && <p className='text-red-600'>{error}</p>}
         </form>
-      <div className='flex justify-between'>
-        <p>Already have an account?</p>
-        <Link href='/login' className='hover:text-orange-500 '>Log In</Link>
-      </div>
+        <div className='flex justify-between'>
+          <p>Already have an account?</p>
+          <Link href='/login' className='hover:text-orange-500'>Log In</Link>
+        </div>
       </div>
     </div>
   );
